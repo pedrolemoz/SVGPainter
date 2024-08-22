5809a2f66e1230bf4159c409f4c44924697e0de9
Pedro Lemos <lemozxpedro@gmail.com>
2024-08-22 03:45:47 -0300

    feat: added fromString and fromAsset constuctors to SvgVisualizer

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:svg_painter/painter.dart';
 
-import 'svg_icons.dart';
+import 'svg_mocks.dart';
 
-void main() => runApp(const UniconsExample());
+void main() => runApp(const Example());
 
-class UniconsExample extends StatelessWidget {
-  const UniconsExample({super.key});
+class Example extends StatelessWidget {
+  const Example({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -28,55 +28,55 @@ class UniconsExample extends StatelessWidget {
 }
 
 class IconsPage extends StatelessWidget {
-  final _iconsLine = [
-    SvgIcons.apps,
-    SvgIcons.cog,
-    SvgIcons.user,
-    SvgIcons.schedule,
-    SvgIcons.phone_volume,
-    SvgIcons.trophy,
-    SvgIcons.edit,
-    SvgIcons.bed,
-    SvgIcons.basketball_hoop,
-    SvgIcons.airplay,
-    SvgIcons.heartbeat,
-    SvgIcons.suitcase,
-    SvgIcons.comment_exclamation,
-    SvgIcons.padlock,
-    SvgIcons.usd_circle,
-    SvgIcons.map_pin,
-    SvgIcons.image_edit,
-    SvgIcons.money_stack,
-    SvgIcons.file_alt,
-    SvgIcons.bus_alt,
-    SvgIcons.book_open,
-    SvgIcons.credit_card,
-    SvgIcons.grin,
-    SvgIcons.newspaper,
-    SvgIcons.accessible_icon_alt,
-    SvgIcons.calculator_alt,
-    SvgIcons.unlock_alt,
-    SvgIcons.search_alt,
-    SvgIcons.package,
-    SvgIcons.tablet,
-    SvgIcons.english_to_chinese,
-    SvgIcons.bars,
-    SvgIcons.battery_empty,
-    SvgIcons.android_alt,
-    SvgIcons.home,
-    SvgIcons.scroll,
-    SvgIcons.car,
-    SvgIcons.paperclip,
-    SvgIcons.university,
-    SvgIcons.medal,
-    SvgIcons.image_upload,
-    SvgIcons.mars,
-    SvgIcons.fahrenheit,
-    SvgIcons.bill,
-    SvgIcons.bitcoin_circle,
-    SvgIcons.camera,
-    SvgIcons.gold,
-    SvgIcons.ninja,
+  final mocks = [
+    SvgMocks.apps,
+    SvgMocks.cog,
+    SvgMocks.user,
+    SvgMocks.schedule,
+    SvgMocks.phone_volume,
+    SvgMocks.trophy,
+    SvgMocks.edit,
+    SvgMocks.bed,
+    SvgMocks.basketball_hoop,
+    SvgMocks.airplay,
+    SvgMocks.heartbeat,
+    SvgMocks.suitcase,
+    SvgMocks.comment_exclamation,
+    SvgMocks.padlock,
+    SvgMocks.usd_circle,
+    SvgMocks.map_pin,
+    SvgMocks.image_edit,
+    SvgMocks.money_stack,
+    SvgMocks.file_alt,
+    SvgMocks.bus_alt,
+    SvgMocks.book_open,
+    SvgMocks.credit_card,
+    SvgMocks.grin,
+    SvgMocks.newspaper,
+    SvgMocks.accessible_icon_alt,
+    SvgMocks.calculator_alt,
+    SvgMocks.unlock_alt,
+    SvgMocks.search_alt,
+    SvgMocks.package,
+    SvgMocks.tablet,
+    SvgMocks.english_to_chinese,
+    SvgMocks.bars,
+    SvgMocks.battery_empty,
+    SvgMocks.android_alt,
+    SvgMocks.home,
+    SvgMocks.scroll,
+    SvgMocks.car,
+    SvgMocks.paperclip,
+    SvgMocks.university,
+    SvgMocks.medal,
+    SvgMocks.image_upload,
+    SvgMocks.mars,
+    SvgMocks.fahrenheit,
+    SvgMocks.bill,
+    SvgMocks.bitcoin_circle,
+    SvgMocks.camera,
+    SvgMocks.gold,
+    SvgMocks.ninja,
   ];
 
   IconsPage({super.key});
@@ -93,13 +93,13 @@ class IconsPage extends StatelessWidget {
       body: GridView.builder(
         padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 30.0),
         shrinkWrap: true,
-        itemCount: _iconsLine.length,
+        itemCount: mocks.length,
         gridDelegate:
             const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 6),
         itemBuilder: (context, index) => Padding(
           padding: const EdgeInsets.all(8),
-          child: SvgVisualizer(
-            svg: _iconsLine[index],
+          child: SvgVisualizer.fromString(
+            svg: mocks[index].data,
             color: colorScheme.primary,
           ),
         ),
--- lib/src/svg_visualizer.dart
+++ lib/src/svg_visualizer.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'svg_painter.dart';
@@ -6,11 +8,42 @@ class SvgVisualizer extends StatelessWidget {
   final String svg;
   final Color? color;
 
-  const SvgVisualizer({
-    super.key,
+  const SvgVisualizer._({
     required this.svg,
     this.color,
   });
+
+  factory SvgVisualizer.fromString({required String svg, Color? color}) =>
+      SvgVisualizer._(svg: svg, color: color);
+
+  static Widget fromAsset(
+    BuildContext context, {
+    required String asset,
+    Color? color,
+    Widget Function(BuildContext context)? onLoading,
+  }) {
+    return FutureBuilder<SvgVisualizer>(
+      future: _loadFromAsset(context, asset: asset, color: color),
+      builder: (context, snapshot) {
+        return switch (snapshot.connectionState) {
+          ConnectionState.done => snapshot.data!,
+          _ => onLoading?.call(context) ?? const SizedBox.shrink(),
+        };
+      },
+    );
+  }
+
+  static Future<SvgVisualizer> _loadFromAsset(
+    BuildContext context, {
+    required String asset,
+    Color? color,
+  }) async {
+    final bundle = DefaultAssetBundle.of(context);
+    final byteData = await bundle.load(asset);
+    final charCodes = byteData.buffer.asUint8List();
+    final svg = String.fromCharCodes(charCodes);
+    return SvgVisualizer._(svg: svg, color: color);
+  }
 
   @override
   Widget build(BuildContext context) {
